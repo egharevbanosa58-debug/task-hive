4ae98dd33ce780a8b49a6beb366941b801a9a744
Nosa <egharevbanosa58@gmail.com>
2026-04-06 22:31:08 -0900

    Just made projects to accomodate multiple tasks and show them

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,10 +1,21 @@
 from . models import Project
 from rest_framework import serializers
 
+from tasks.models import Task
+
 # Serializers go here
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'status', 'created_at']
 
 class ProjectSerializer(serializers.ModelSerializer):
+    
+    # This magic line pulls in all tasks linked to this project
+    # This 'tasks' variable must match the 'related_name' in your Task model
+    tasks = TaskSerializer(many=True, read_only=True)
+
     class Meta:
         model = Project
-        fields = ['id', 'name', 'description', 'created_at']
+        fields = ['id', 'name', 'description', 'created_at', 'tasks']
         read_only_fields = ['id', 'created_at']
